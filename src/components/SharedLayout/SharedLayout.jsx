e4cd1bbc2d81aa6eb4a6aa42add70bbe82d66d88
Andrii_Kuzhelnii <remontdiskov1979@gmail.com>
2024-03-22 16:53:18 +0200

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router";
-import Header from "../Header/Header"
+import Header from "../Header/Header";
+// import Header from "../Header/Header"
 export default function SharedLayout() {
 	return (
 		<>
-<Header/>
-			<main>
+		<Header/>
+		<main>
 				<Suspense fallback={<h1>Load...</h1>}>{<Outlet />}</Suspense>
 			</main>
 		</>
