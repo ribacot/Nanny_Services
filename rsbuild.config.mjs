d2b40e76c047fa800c6c84ad30be0bbe4ed5259c
Andrii_Kuzhelnii <remontdiskov1979@gmail.com>
2024-03-24 23:18:17 +0200

    Update rsbuild.config.mjs

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginImageCompress } from "@rsbuild/plugin-image-compress";
 import { pluginStylus } from "@rsbuild/plugin-stylus";
-// import { pluginBabel } from "@rsbuild/plugin-babel";
+import { pluginBabel } from "@rsbuild/plugin-babel";
 
 export default defineConfig({
-	plugins: [pluginReact(), pluginImageCompress(), pluginStylus()],
+	plugins: [pluginReact(), pluginImageCompress(), pluginStylus(), pluginBabel()],
 	html: { template: "./public/index.html" },
+	output: {
+		distPath: {
+			root: "build",
+		},
+	},
 });
