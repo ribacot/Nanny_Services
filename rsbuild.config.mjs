7face77c8bf781ee3d5cbe5177535fd30a6bc8e2
Andrii_Kuzhelnii <remontdiskov1979@gmail.com>
2024-03-27 22:53:36 +0200

    rebuilt

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-import { pluginImageCompress } from "@rsbuild/plugin-image-compress";
-import { pluginStylus } from "@rsbuild/plugin-stylus";
-import { pluginBabel } from "@rsbuild/plugin-babel";
 
 export default defineConfig({
-	plugins: [pluginReact(), pluginImageCompress(), pluginStylus(), pluginBabel()],
-	html: { template: "./public/index.html" },
-	output: {
-		distPath: {
-			root: "dist",
-		},
+	plugins: [pluginReact()],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["postcss-loader"],
+				type: "css",
+			},
+		],
 	},
 });
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router";
+import { Header } from "../Header/Header";
 export function SharedLayout() {
 	return (
 		<>
+		<Header/>
 			<main>
-				<Suspense fallback={<h1>Loading...</h1>}>{<Outlet />}</Suspense>
+				<Suspense fallback={<h1>Load...</h1>}>{<Outlet />}</Suspense>
 			</main>
 		</>
 	);
